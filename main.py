f0e1982b07ff484f6db5bd887356c306383be473
Yash Lala <yashlala@gmail.com>
2021-05-24 15:04:49 -0700

    Convert all .py files to unix, format them with black.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-#coding:utf-8
+# coding:utf-8
 
 #
 # two tube model, draw frequency response and waveform, considering glottal voice source and mouth radiation
@@ -14,45 +14,54 @@ from glottal import *
 from HPF import *
 from utils import *
 
-if __name__ == '__main__':
-	# Length & Area value, from problems 3.8 in "Digital Processing of Speech Signals" by L.R.Rabiner and R.W.Schafer
-	with open("config.json", 'r') as f:
-		config = json.load(f)
-	params = config["parameters"]
+if __name__ == "__main__":
+    # Length & Area value, from problems 3.8 in "Digital Processing of Speech Signals" by L.R.Rabiner and R.W.Schafer
+    with open("config.json", "r") as f:
+        config = json.load(f)
+    params = config["parameters"]
 
-	glo=Class_Glottal(params["glottal_tclosed"], params["glottal_trise"], params["glottal_tfall"], params["sampling_rate"])
-	hpf=Class_HPF(params["hpf_fc"], params["sampling_rate"])
+    glo = Class_Glottal(
+        params["glottal_tclosed"],
+        params["glottal_trise"],
+        params["glottal_tfall"],
+        params["sampling_rate"],
+    )
+    hpf = Class_HPF(params["hpf_fc"], params["sampling_rate"])
 
-	fig = plt.figure()
-	fig_rows = len(config["tube_values"])
-	fig_cols = 2
+    fig = plt.figure()
+    fig_rows = len(config["tube_values"])
+    fig_cols = 2
 
-	i = 1
+    i = 1
 
-	output_waveforms = {}
+    output_waveforms = {}
 
-	output_waveforms["source"] = glo.yg_repeat.tolist()
-	save_wav(glo.yg_repeat, 'source' + '.wav', params["sampling_rate"])
-	for (key, val) in config["tube_values"].items():
-		twotube = Class_NTube(val, params["rg0"], params["rl0"], params["sampling_rate"], params["C0"])
+    output_waveforms["source"] = glo.yg_repeat.tolist()
+    save_wav(glo.yg_repeat, "source" + ".wav", params["sampling_rate"])
+    for (key, val) in config["tube_values"].items():
+        twotube = Class_NTube(
+            val, params["rg0"], params["rl0"], params["sampling_rate"], params["C0"]
+        )
 
-		#plt.subplot(fig_rows,fig_cols,i)
-		plot_freq_res(twotube, '/' + key + '/', glo, hpf)
-		#plt.subplot(fig_rows,fig_cols,i+1)
-		yout = plot_waveform(twotube, '/' + key + '/', glo, hpf)
-		
-		output_waveforms[key] = yout.tolist()
+        # plt.subplot(fig_rows,fig_cols,i)
+        plot_freq_res(twotube, "/" + key + "/", glo, hpf)
+        # plt.subplot(fig_rows,fig_cols,i+1)
+        yout = plot_waveform(twotube, "/" + key + "/", glo, hpf)
 
-		save_wav(yout, 'yout_' + key + '.wav', params["sampling_rate"])  # save generated waveform as a wav file
+        output_waveforms[key] = yout.tolist()
 
-		i += 2
+        save_wav(
+            yout, "yout_" + key + ".wav", params["sampling_rate"]
+        )  # save generated waveform as a wav file
 
-	output = {
-		"sampling_rate": params["sampling_rate"],
-		"output_waveforms": output_waveforms
-	}
-	with open('output.json', 'w') as f:
-		json.dump(output, f)
+        i += 2
 
-	#fig.tight_layout()
-	#plt.show()
+    output = {
+        "sampling_rate": params["sampling_rate"],
+        "output_waveforms": output_waveforms,
+    }
+    with open("output.json", "w") as f:
+        json.dump(output, f)
+
+    # fig.tight_layout()
+    # plt.show()
