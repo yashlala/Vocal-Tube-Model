94d77b5d2d8320685245095dc216cd75f7d0abd3
Yash Lala <yashlala@gmail.com>
2021-05-29 23:54:03 -0700

    Begin rewriting `main.py` for hyperparameter search.
    - Move `utils.py` methods into `main.py`. Clean up their code in the
      process.
    - Add comments to all model components in `main.py`.

--- main.py
+++ main.py
@@ -15,25 +15,73 @@ from glottal import Glottal
 from high_pass_filter import HPF
 
 
+def gen_waveform(twotube, glo, hpf):
+    """Generate a sonic waveform given model components.
+
+    Arguments:
+        - A glottal model (simulates source).
+        - A 2-tube model (simulates filter).
+        - A high-pass filter model (simulates post-mouth).
+    """
+    yg_repeat = glo.get_output()
+    y2tm = twotube.process(yg_repeat)
+    return hpf.iir1(y2tm)
+
+
+def save_wav(waveform, file_path, sampling_rate):
+    """Save the input waveform to the given file path as a .wav file.
+
+    The waveform is looped, for ease of listening.
+    """
+    waveform_data = (waveform * 2 ** 15).astype(np.int16)
+    wavwrite(file_path, sampling_rate, waveform_data)
+
+
+# TODO: pull out of the parameters out of JSON form.
+#       Just loop over them in this script.
+#
+# glottal_parameters =  {
+#         "rg0": 0.95,
+#         "rl0": 0.9,
+#         "sampling_rate": 48000,
+#         "C0": 35000.0,
+#         "glottal_tclosed": 2.5,
+#         "glottal_trise": 3.0,
+#         "glottal_tfall": 1.0,
+#         "hpf_fc": 2000
+#         }
+#
+# 	"tube_values" : {
+# 		"random": [[5, 1], [4, 2]],
+# 		"a": [[9, 1], [8, 7]],
+# 		"ae": [[4, 1], [13, 8]],
+# 		"i": [[9, 8], [6, 1]],
+# 		"u": [[10, 7], [7, 3]],
+# 		"o": [[9, 1], [8, 7], [5.6, 3]]
+# 	}
+# }
+
+
 if __name__ == "__main__":
-    # Length & Area value, from problems 3.8 in "Digital Processing of Speech
-    # Signals" by L.R.Rabiner and R.W.Schafer
     with open("config.json", "r") as f:
         config = json.load(f)
     params = config["parameters"]
 
+    # Define our Glottal emitter. This models the "source" of the sound.
+    # As per the source-filter model, its frequency should not significantly
+    # affect the pitch of the resulting sound (although this remains to be
+    # confirmed). We keep it constant for all vowel runs.
     glo = Glottal(
         params["glottal_tclosed"],
         params["glottal_trise"],
         params["glottal_tfall"],
         params["sampling_rate"],
     )
+
+    # Define our high-pass filter. This models the transformations that occur
+    # to sounds as/after they are leaving the human mouth. We don't optimize
+    # over these parameters, either.
     hpf = HPF(params["hpf_fc"], params["sampling_rate"])
-
-    fig = plt.figure()
-    fig_rows = len(config["tube_values"])
-    fig_cols = 2
-
 
     output_waveforms = {}
 
@@ -53,7 +101,6 @@ if __name__ == "__main__":
         save_wav(
             yout, "yout_" + key + ".wav", params["sampling_rate"]
         )  # save generated waveform as a wav file
-
 
     output = {
         "sampling_rate": params["sampling_rate"],
